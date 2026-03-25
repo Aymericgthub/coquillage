44c23329d8361fd5a9dd41161d13732f9f32ae32
Aymeric <ayikoe.aymeric@gmail.com>
2026-03-25 09:36:08 +0100

    modified:   app/templates/index.html

--- app/app.py
+++ app/app.py
@@ -1,7 +1,28 @@
 
-from app import create_app
+@app.route("/artists", methods=["GET", "POST"])
+def show_artists():
+    artists = None
+    if request.method == "POST":
+        query = request.form.get("query")
+        conn = mariadb.connect(
+            user=DB_USER,
+            password="uimm",
+            host="localhost",  # ou le nom du service mysql/mariadb du docker-compose
+            port=3306,
+            database="tourneur")
+        cursor = conn.cursor(dictionary=True)
+        print(f"SELECT * FROM artiste WHERE nom LIKE '{query}'")
+        # cursor.execute(f"SELECT * FROM artiste WHERE nom LIKE '{query}'")
+        cursor.execute("SELECT * FROM artiste WHERE nom LIKE %s", (f"{query}",))
+        result = cursor.fetchall()
+        artists = [artist["nom"] for artist in result]
+    return render_template("index.html", artists=artists)
 
-app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#from app import create_app
+
+#app = create_app()
+
+#if __name__ == "__main__":
+   # app.run(debug=True)
+
